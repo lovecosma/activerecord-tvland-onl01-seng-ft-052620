d304955a470796bded669caa3685ba88d452e2b1
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-28 19:19:16 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,15 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+  self.first_name + " " + self.last_name
+  end
+
+  def list_roles
+    roles_and_shows = []
+    self.shows.each do |show|
+      roles_and_shows << show.characters.name + " - " + self.shows.name
+     end
+  end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -2,9 +2,35 @@ require "bundler/setup"
 
 require "sinatra/activerecord"
 
+require_relative '../app/models/character.rb'
+require_relative '../app/models/show.rb'
+require_relative '../app/models/actor.rb'
+require_relative '../app/models/network.rb'
+
 Bundler.require
 
 Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
 
 connection_details = YAML::load(File.open('config/database.yml'))
 ActiveRecord::Base.establish_connection(connection_details)
+
+sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS actors (
+  id INTEGER PRIMARY KEY,
+  first_name TEXT,
+  last_name TEXT
+  )
+SQL
+
+ActiveRecord::Base.connection.execute(sql)
+
+sql2 = <<-SQL
+  CREATE TABLE IF NOT EXISTS characters (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  actor_id INTEGER,
+  show_id INTEGER
+  )
+SQL
+
+ActiveRecord::Base.connection.execute(sql2)
